023482120a50c7c99bea5a823a32e52d0e509538
kentr0w <qbextted0@gmail.com>
2019-05-01 21:25:55 +0300

    Correct Input

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,8 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.*;
 import javafx.geometry.Insets;
@@ -41,7 +43,7 @@ public class Main extends Application {
 
 
 
-    static double WIDTH, HEIGHT;
+    static double  WIDTH, HEIGHT;
     private Scene scene;
     PerspectiveCamera camera;
     private Rotate rotateX = new Rotate(0, Rotate.X_AXIS);
@@ -52,9 +54,10 @@ public class Main extends Application {
     int b = -40;
     int c = -100;
     private Group root, root1;
-    int count = 13;
+    int count = 4;
     public SubScene subScene;
     public Scene sceneResult;
+    PhongMaterial phongMaterial;
 
     private List<Box> boxes = new ArrayList<>();
 
@@ -81,52 +84,58 @@ public class Main extends Application {
         root1.getChildren().add(strings);
 
         double x,y,z;
-        int step=5;
-        y=z=0;
-        x =step;
-        PhongMaterial phongMaterial = new PhongMaterial();
+        int step=5, k_x=0, k_y=0, k_z=0;
+        y=z=step/1.9;
+        x =step/1.9;
+        phongMaterial = new PhongMaterial();
         phongMaterial.setDiffuseColor(Color.GREEN);
 
-        for(int i=1; i<count*count*count+1; ++i){
+        for(int i=0; i<count*count*count; ++i){
             Box obj = new Box(4,4,4);
             obj.setOnMousePressed((MouseEvent e)->{
                 if(obj.getMaterial() == (null))
                     obj.setMaterial(phongMaterial);
+                else{
+                    obj.setMaterial(null);
+                }
             });
-            if(i==1) {
-                root.getChildren().add(obj);
-                boxes.add(obj);
-                continue;
+            if(i!=0 && i%count==0){
+                    if (y > 0)
+                        y = -y;
+                    else {
+                        y = -y;
+                        y += step;
+                        ++k_y;
+                    }
+                    if (k_y + 1 > count / 2) {
+                        y = step / 1.9;
+                        k_y = 0;
+                    }
             }
-
-            if(i%count==0){
-                if(y>0)
-                    y=-y;
+            if(i!=0 && i%(count*count)==0){
+                if (y > 0)
+                    y = -y;
                 else {
-                    y=-y;
+                    y = -y;
                     y += step;
+                    ++k_y;
                 }
-                if(y/step>count/2)
-                    y=0;
-            }
-            if(i%(count*count)==0){
-                if(y>0)
-                    y=-y;
-                else {
-                    y=-y;
-                    y += step;
+                if (k_y + 1 > count / 2) {
+                    y = step / 1.9;
+                    k_y = 0;
                 }
-                if(y/step>count/2)
-                    y=0;
 
                 if(z>0)
                     z=-z;
                 else {
                     z=-z;
                     z += step;
+                    ++k_z;
                 }
-                if(z/step>count/2)
-                    z=0;
+                if (k_z + 1 > count / 2) {
+                    z = step / 1.9;
+                    k_z = 0;
+                }
             }
 
             obj.setTranslateX(x);
@@ -139,9 +148,12 @@ public class Main extends Application {
             else {
                 x=-x;
                 x += step;
+                ++k_x;
             }
-            if(x/step>count/2)
-                x=0;
+            if(k_x+1>count/2) {
+                x = step/1.9;
+                k_x=0;
+            }
         }
 
         final PhongMaterial redMaterial = new PhongMaterial();
@@ -169,7 +181,7 @@ public class Main extends Application {
         root.getChildren().addAll(xAxis, yAxis, zAxis);
 
         // Use a SubScene
-        subScene = new SubScene(root, WIDTH/2-100, HEIGHT/2 +100, true,
+        subScene = new SubScene(root, (3*WIDTH)/4, HEIGHT, true,
                 SceneAntialiasing.BALANCED);
         subScene.setFill(Color.PINK);
         subScene.setCamera(camera);
@@ -250,22 +262,51 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
 
-        WIDTH = Screen.getPrimary().getBounds().getWidth();
-        HEIGHT = Screen.getPrimary().getBounds().getHeight();
+        WIDTH = Screen.getPrimary().getBounds().getWidth()/2;
+        HEIGHT = Screen.getPrimary().getBounds().getHeight()/2;
 
-        SubScene scene = (createContent());
+
+
         BorderPane pane = new BorderPane();
-        pane.setCenter(scene);
+
+        SubScene subscene = (createContent());
+        pane.setLeft(subscene);
+
+
         TextArea textArea = new TextArea();
-
-        pane.setStyle("-fx-background-color: #008080;");
-
         textArea.setPrefHeight(10);
         textArea.setPrefWidth(100);
 
         Button select = new Button("Reset");
         select.setOnAction(e->{
-            System.out.println(sceneResult.getHeight());
+        });
+
+        Button color = new Button("Color");
+        color.setOnAction(event -> {
+        });
+
+
+        CheckBox view = new CheckBox("View Mood");
+        view.setOnAction(event -> {
+            if(view.isSelected()) {
+                for (Box obj : boxes) {
+                    obj.setOnMousePressed(event1 -> {
+
+                    });
+                }
+                System.out.println("now dont working");
+            }
+            else{
+                for (Box obj : boxes) {
+                    obj.setOnMousePressed(event1 -> {
+                        if(obj.getMaterial() == (null))
+                            obj.setMaterial(phongMaterial);
+                        else
+                            obj.setMaterial(null);
+                    });
+                }
+                System.out.println("now  working");
+            }
         });
 
         Button zoom_plus = new Button("Zoom +");
@@ -280,43 +321,78 @@ public class Main extends Application {
         });
         zoom_min.setPrefSize(75,25);
 
-        AnchorPane.setTopAnchor(textArea, 100.0);
-        AnchorPane.setRightAnchor(textArea, WIDTH/13);
-
-        AnchorPane.setTopAnchor(select, 160.0);
-        AnchorPane.setRightAnchor(select, 120.0);
-
-        AnchorPane.setTopAnchor(zoom_plus, 480.0);
-        AnchorPane.setRightAnchor(zoom_plus, 108.0);
-
-        AnchorPane.setTopAnchor(zoom_min, 480.0);
-        AnchorPane.setRightAnchor(zoom_min, 30.0);
 
 
 
-        AnchorPane anchorPane = new AnchorPane(textArea, select,zoom_plus,zoom_min);
+        AnchorPane anchorPane = new AnchorPane(textArea, select, zoom_min, zoom_plus, color, view);
+        anchorPane.setStyle("-fx-background-color: #008080;");
+        anchorPane.setPrefSize((WIDTH/4),HEIGHT);
 
-        anchorPane.setPrefSize(170,150);
+        AnchorPane.setTopAnchor(textArea, HEIGHT - 350);
+        AnchorPane.setRightAnchor(textArea, 45.0);
+
+        AnchorPane.setTopAnchor(select, HEIGHT-300);
+        AnchorPane.setRightAnchor(select, 68.0);
+
+        AnchorPane.setTopAnchor(color, HEIGHT-220);
+        AnchorPane.setRightAnchor(color, 68.0);
+
+        AnchorPane.setTopAnchor(view, HEIGHT - 170);
+        AnchorPane.setRightAnchor(view, 48.0);
+
+        AnchorPane.setTopAnchor(zoom_plus, HEIGHT - zoom_plus.getPrefHeight()-20);
+        AnchorPane.setRightAnchor(zoom_plus, 100.0);
+
+        AnchorPane.setTopAnchor(zoom_min, HEIGHT-zoom_min.getPrefHeight()-20);
+        AnchorPane.setRightAnchor(zoom_min, 15.0);
+
 
         pane.setRight(anchorPane);
-        pane.setPrefSize(700,700);
+        pane.setPrefSize(WIDTH,HEIGHT);
+
 
         sceneResult = new Scene(pane);
 
-
+        sceneResult.widthProperty().addListener(new ChangeListener<Number>() {
+            int b =0;
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                int a = newValue.intValue();
+                if(b!=0) {
+                    WIDTH+=(a-b);
+                    subscene.setWidth(subscene.getWidth() + (a-b));
+                }
+                b = a;
+            }
+        });
+        sceneResult.heightProperty().addListener(new ChangeListener<Number>() {
+            int d=0;
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                int c = newValue.intValue();
+                if(d!=0) {
+                    HEIGHT+=(c-d);
+                    subscene.setHeight(subscene.getHeight() + (c - d));
+                    for(Node obj : anchorPane.getChildren()){
+                        AnchorPane.setTopAnchor(obj, AnchorPane.getTopAnchor(obj) + (c-d)/2.5);
+                    }
+                    anchorPane.setPrefHeight(anchorPane.getHeight() + (c-d));
+                }
+                d=c;
+            }
+        });
 
         primaryStage.setTitle("3D");
         primaryStage.setResizable(false);
         handleMouseEvents(primaryStage);
         primaryStage.setScene(sceneResult);
 
-        System.out.println(sceneResult.getFill().toString());
-
         Rectangle2D screenBounds = Screen.getPrimary().getBounds();
         System.out.println(screenBounds);
+
         primaryStage.setResizable(true);
-        primaryStage.setMinHeight(HEIGHT/2+100);
-        primaryStage.setMinWidth(WIDTH/2);
+        primaryStage.setMinHeight(HEIGHT);
+        primaryStage.setMinWidth(WIDTH);
         primaryStage.show();
 
     }
@@ -325,3 +401,64 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+/*
+for(int i=0; i<count*count; ++i){
+        Box obj = new Box(4,4,4);
+        obj.setOnMousePressed((MouseEvent e)->{
+        if(obj.getMaterial() == (null))
+        obj.setMaterial(phongMaterial);
+        else{
+        }
+        });
+        if(i==1) {
+        root.getChildren().add(obj);
+        boxes.add(obj);
+        continue;
+        }
+
+        if(i!=0 && i%count==0){
+        if(y>0)
+        y=-y;
+        else {
+        y=-y;
+        y += step;
+        }
+        if(y/step>count/2)
+        y=0;
+        }
+        if(i!=0 && i%(count*count)==0){
+        if(y>0)
+        y=-y;
+        else {
+        y=-y;
+        y += step;
+        }
+        if(y/step>count/2)
+        y=0;
+
+        if(z>0)
+        z=-z;
+        else {
+        z=-z;
+        z += step;
+        }
+        if(z/step>count/2)
+        z=0;
+        }
+
+        obj.setTranslateX(x);
+        obj.setTranslateY(y);
+        obj.setTranslateZ(z);
+        boxes.add(obj);
+        root.getChildren().add(obj);
+        if(x>0)
+        x=-x;
+        else {
+        x=-x;
+        x += step;
+        }
+        if(x/step>count/2)
+        x=0;
+        }
+        */
